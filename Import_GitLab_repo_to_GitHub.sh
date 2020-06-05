7eef3d4c811205676087922744d0bb3d46e5c2e1
Thomas de Barros <thomas.de-barros@epitech.eu>
2020-06-06 00:13:43 +0200

    basic script with user confirmation, not functional yet

---
+++ Import_GitLab_repo_to_GitHub.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Clear your terminal to enhance visibility
+clear
+
+while [ "$confirmation" != "Y" ]
+do
+    echo "The script will download and load files from the current directory, do you want to continue?"
+    echo "[Y/N]"
+    echo -e "> \c"
+
+    read confirmation
+    case "$confirmation" in 
+        y|Y ) clear
+              break;;
+        n|N ) echo "Thanks for using the script, see you soon :)"
+              exit;;
+        # In case of bad input
+        * ) clear;;
+    esac 
+done
+
+# Continue
+echo "Please copy/paste here the GitLab repository you want to import to GitHub"
+echo -e "> \c"
+read repo_link
+clear
+
+echo "What will be the name of the repository on GitHub ?"
+echo -e "> \c"
+read repo_name
+clear
+
+echo "Now trying to git clone from GitLab..."
+#git clone $repo_link
+
+echo "Now trying to init a git repository..."
+mkdir $repo_name
+cd $repo_name 
+## check if the repository already exists, potentially leading to an error
+touch file
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # FromGitLabToGitHub
 A shell script made to easily import your GitLab repositories to GitHub
+
+Just follow the instructions, it's that simple !
